5d49a2e5f5116dac8e23ca9b5bba5f762595a691
Daniyal Parveez <dparveez@rudderstack.com>
2022-12-26 22:12:35 +0530

    Changes.

--- benchmarks/openfaas/function-invocation/load_test.js
+++ benchmarks/openfaas/function-invocation/load_test.js
@@ -1,6 +1,5 @@
+import { check } from "k6";
 import http from "k6/http";
-import { check, sleep } from "k6";
-import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.2.0/index.js";
 
 import uuid from "./uuid.js";
 
@@ -10,106 +9,118 @@ export const options = {
       executor: "constant-arrival-rate",
 
       // Our test should last X seconds in total
-      duration: "1h",
+      duration: "1m",
 
       // It should start X iterations per `timeUnit`. Note that iterations starting points
       // will be evenly spread across the `timeUnit` period.
-      rate: 50,
+      rate: 5,
 
       // It should start `rate` iterations per second
       timeUnit: "1s",
 
       // It should preallocate X VUs before starting the test
-      preAllocatedVUs: 50,
+      preAllocatedVUs: 10,
 
       // It is allowed to spin up to X maximum VUs to sustain the defined
       // constant arrival rate.
-      maxVUs: 600
+      maxVUs: 100
     }
   }
 };
 
-const workspaceId = __ENV.WORKSPACE_ID || "2At61jJxm49VSaIxI6KCECSZfCz";
+const workspaceId = __ENV.WORKSPACE_ID || "2JIryJwEQVBhtXihKzFarELIX7X";
 const hostUrl =
-  __ENV.HOST_URL || "http://transformer.dparveez.svc.cluster.local:80";
-const url = hostUrl + "/customTransform";
+  __ENV.HOST_URL ||
+  "http://transformer.rudder-us-east-1a-blue.svc.cluster.local:80";
+const url = `${hostUrl}/customTransform`;
 
 const versionIds = [
-  "2Gf1RoOzkFIfhEogL276zcw5nEe",
-  "2Gf1xsaLEXpD3l9ysELjJAnMWuP",
-  "2Gf26VFX6nZGUmsqajEgLPpZl6r",
-  "2Gf2D3BpP1RacXKNI7Cp6utDnU5",
-  "2Gf2KIWPpmiLmeSimPiAgOUg54r",
-  "2Gf2VukAaiFXzkbypwFYzvgxDXd",
-  "2Gf2eQXfEKPDIwZFZZRyqnWkHv8"
+  "2JJBHfFshI2kXdgVBVW0h73zq0u",
+  "2JIyGhcqALLnylXdS3Xl5Sfq0I5",
+  "2JIyMRR8JqyR4j6Ys5a8weK5U0N",
+  "2JIyRdzwav3MW4KgzHnfYpIJwX5",
+  "2JIyW2FBbZWjiyC3Zm3Woc2TWx",
+  "2JIyZiseYSwFcrKzQ2eG4RcByea",
+  "2JIye7Kc2JJjh8I77xDesRJNIvs",
+  "2JIyRdzwav3MW4KgzHnfYpIJwX6",
+  "2JIyRdzwav3MW4KgzHnfYpIJwX7",
+  "2JIyRdzwav3MW4KgzHnfYpIJwX8"
 ];
 
 const maxBatchSize = parseInt(__ENV.MAX_BATCH_SIZE || 200);
 
-const base_msg = JSON.parse(open("./base_msg.json"));
+const base_msgs = JSON.parse(open("./base_msgs.json"));
 const base_meta = JSON.parse(open("./base_meta.json"));
 const base_dst = JSON.parse(open("./base_dst.json"));
 
 export function setup() {
-  const msgs = [];
-  const metas = [];
-  const dests = [];
+  const payloads = [];
 
-  for (let i = 0; i < maxBatchSize; i++) {
-    const uuid4 = uuid.v4();
+  const baseMsgs = JSON.parse(JSON.stringify(base_msgs));
+  const minMsgsPerBaseType = parseInt(maxBatchSize / baseMsgs.length, 10);
 
-    let msg = JSON.parse(JSON.stringify(base_msg));
-    msg.messageId = uuid4;
-    msg.randomId = uuid4;
-    msgs.push(msg);
+  for (let i = 0; i < baseMsgs.length; i++) {
+    const msgDupCount =
+      i === baseMsgs.length - 1
+        ? minMsgsPerBaseType
+        : maxBatchSize - payloads.length;
+
+    for (let j = 0; j < msgDupCount; j++) {
+      const msg = JSON.parse(JSON.stringify(baseMsgs[i]));
+
+      const meta = JSON.parse(JSON.stringify(base_meta));
+      meta.workspaceId = workspaceId;
+      meta.sourceId = `s${i}`;
+      meta.sourceDefinitionId = `sD${i}`;
+      meta.sourceType = `sT${i}`;
+      meta.destinationId = `d${i}`;
+      meta.destinationDefinitionId = `dD${i}`;
+      meta.destinationType = `dT${i}`;
+
+      const dest = JSON.parse(JSON.stringify(base_dst));
+
+      const payload = {};
+      payload.message = msg;
+      payload.metadata = meta;
+      payload.destination = dest;
+
+      payloads.push(payload);
+    }
   }
 
-  versionIds.forEach((versionId, i) => {
-    let meta = JSON.parse(JSON.stringify(base_meta));
-    meta.workspaceId = workspaceId;
-    meta.sourceId = "s" + i;
-    meta.sourceDefinitionId = "sD" + i;
-    meta.sourceType = "sT" + i;
-    meta.destinationId = "d" + i;
-    meta.destinationDefinitionId = "dD" + i;
-    meta.destinationType = "dT" + i;
-
-    metas.push(meta);
-
-    let dest = JSON.parse(JSON.stringify(base_dst));
-    dest.Transformations[0].VersionID = versionId;
-    dests.push(dest);
-  });
-
-  const payloadsByVersionId = {};
-
-  versionIds.forEach((versionId, i) => {
-    payloadsByVersionId[versionId] = [];
-
-    msgs.forEach(msg => {
-      let payload = {};
-      payload.message = msg;
-      payload.metadata = metas[i];
-      payload.destination = dests[i];
-
-      payloadsByVersionId[versionId].push(payload);
-    });
-  });
-
-  return { payloadsByVersionId };
+  return { payloads };
 }
 
 export default function(data) {
   const requests = [];
 
   versionIds.forEach((versionId, i) => {
-    const size = randomIntBetween(10, maxBatchSize);
+    // const size = randomIntBetween(150, maxBatchSize);
+    // const direction = randomItem([-1, 1]);
+
+    // let payloads;
+
+    // if (direction === 1) {
+    //   payloads = data.payloads.slice(0, maxBatchSize);
+    // } else {
+    //   payloads = data.payloads
+    //     .slice()
+    //     .reverse()
+    //     .slice(0, maxBatchSize);
+    // }
+    const { payloads } = data;
+
+    for (let j = 0; j < payloads.length; j++) {
+      const uuid4 = uuid.v4();
+      payloads[j].message.messageId = uuid4;
+      payloads[j].metadata.messageId = uuid4;
+      payloads[j].destination.Transformations[0].VersionID = versionId;
+    }
+
     const req = {
       method: "POST",
-      url: url,
-      body: JSON.stringify(
-        data.payloadsByVersionId[versionId].slice(10, maxBatchSize)
-      ),
+      url,
+      body: JSON.stringify(payloads),
       params: {
         headers: { "Content-Type": "application/json" }
       }
