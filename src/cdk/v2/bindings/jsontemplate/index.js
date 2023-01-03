b8d0105f2e8d51239fc724057cfa46a98a25559d
Jayc <mjayachand@gmail.com>
2023-01-03 13:34:03 +0530

    test libraries section

--- src/cdk/v2/bindings/jsontemplate/index.js
+++
@@ -1,7 +0,0 @@
-const genericFieldMapping = require("../../../../v0/util/data/GenericFieldMapping.json");
-
-exports.getGenericPaths = (key, separator = "??") => {
-  return genericFieldMapping[key]
-    .map(path => `.${path}`)
-    .join(` ${separator} `);
-};
